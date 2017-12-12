7fecaf4ab3a2f1bee4f8389c536791094f93c61c
Joshua Dror <jdror2@gmail.com>
2017-12-12 23:07:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ var index = require("../index.js")
 
 /*
 describe('what-is-a-test', () => {
-  describe('Name', () => {
+  describe('Name', ("Joe") => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
@@ -23,4 +23,3 @@ describe('what-is-a-test', () => {
   })
 })
 */
-
